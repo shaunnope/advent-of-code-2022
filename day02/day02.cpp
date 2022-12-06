64623bdd809cf85f90f8796389cce57cdb49f717
shaunnope <seanyap123@gmail.com>
2022-12-06 03:47:48 +0200

    Combined Day 2 tasks

--- day02/day02.cpp
+++ day02/day02.cpp
@@ -18,8 +18,11 @@ using namespace std;
 using ll = long long;
 using str = string;
 
-int main() {
-    ifstream infile("input.txt");
+const str INPUT = "input.txt";
+
+void task1() {
+    cout << "Task 1: ";
+    ifstream infile(INPUT);
 
     char a, b;
     ll score = 0;
@@ -43,13 +46,52 @@ int main() {
             stream >> a >> b;
             opp = (ll) (a-'A');
             self = (ll) (b-'X');
-            // cout << line << " " << (self+opp+2)%3 << " " << 3*self << endl;
-            score += (self+opp+2)%3 +1 + 3*self ;
+            score += self+1 + 3*((self-opp+4)%3);
     }
 
     infile.close();
 
 
     cout << score << endl;
+}
+
+void task2() {
+    cout << "Task 2: ";
+    ifstream infile(INPUT);
+
+    char a, b;
+    ll score = 0;
+    ll self, opp;
+    /*
+        0 1 2
+      0 1 0 2
+      1 2 1 0
+      2 0 2 1
+
+        0  1  2
+      0 0  2  1
+      1 1  0  2
+      2 2  1  0
+    */
+
+    str line;
+    
+    while(getline(infile, line)) {
+            stringstream stream(line);
+            stream >> a >> b;
+            opp = (ll) (a-'A');
+            self = (ll) (b-'X');
+            score += (self+opp+2)%3 +1 + 3*self;
+    }
+
+    infile.close();
+
+
+    cout << score << endl;
+}
+
+int main() {
+    task1();
+    task2();
 }
 
